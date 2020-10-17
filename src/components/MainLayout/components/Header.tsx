ef601b828a1cbd23e3ed1c17901ec9617a76c53e
Igor Tsialiuk <igor.tsialiuk@burberry.com>
2020-10-17 17:31:09 +0300

    Task-2 (2.1): Configure S3 bucket, build and manually upload, create a CloudFront distribution, create CloudFront distribution invalidation

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">My Store!</Link>
+          <Link className={classes.homeLink} to="/">My Store! (Igor Tsialiuk)</Link>
         </Typography>
 
         {auth && (
