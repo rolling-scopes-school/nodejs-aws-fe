07bdfafaec9717ddb8f96c07f40804f3a19e0df5
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2020-10-20 16:17:30 +0300

    configure scripts for cloudFront

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -8,7 +8,7 @@ import Header from "components/MainLayout/components/Header";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © IvanPresmytsky'}
+      {'Copyright © IvanPresmytsky '}
       <Link color="inherit" href="https://material-ui.com/">
         Beer Store
       </Link>{' '}
