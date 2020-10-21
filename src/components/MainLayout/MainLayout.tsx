cbd367df987f41f6099021118a721a2b2a4bd31a
Maksim Severukhin <mousemckill@gmail.com>
2020-10-21 22:46:12 +0300

    task-2: deploy app to aws

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        Maksim
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,10 +1,11 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export type Product = {
-  id: string,
-  title: string,
-  description: string,
-  price: number,
+  image: string;
+  id: string;
+  title: string;
+  description: string;
+  price: number;
 };
 
 export const ProductSchema = Yup.object().shape({
