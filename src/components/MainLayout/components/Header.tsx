ed8328b175d5d9986261debf6a3b2fceaae82d41
VictoriaMironova <vicky.mironova@gmail.com>
2020-10-24 18:05:07 +0300

    closes task-2

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
     homeLink: {
-      color: 'white',
+      color: 'pink',
       textDecoration: 'none'
     }
   }),
@@ -46,7 +46,7 @@ export default function Header() {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">My Store!</Link>
+          <Link className={classes.homeLink} to="/">Vicky Best Store!</Link>
         </Typography>
 
         {auth && (
