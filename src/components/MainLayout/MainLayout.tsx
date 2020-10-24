df436d78052fb3951ced728f53df329a4c273197
Viktoryia_Yalavaya <Viktoryia_Yalavaya@epam.com>
2020-10-24 23:38:59 +0300

    feature/Add serverless.yml

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        Raccoon Store
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
