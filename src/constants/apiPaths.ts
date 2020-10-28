c31098af49117693fcf0245f3f8e2023e3d946bf
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2020-10-28 21:40:59 +0300

    get products from back-end

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://sy6ni7vd5i.execute-api.us-east-1.amazonaws.com/dev',
+  order: 'https://sy6ni7vd5i.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://sy6ni7vd5i.execute-api.us-east-1.amazonaws.com/dev',
+  bff: 'https://sy6ni7vd5i.execute-api.us-east-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
