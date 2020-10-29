3f60b3187b438fc34f8d8e993784810b87dc5cf8
Denys Kovalishyn <Denys_Kovalishyn@epam.com>
2020-10-29 16:56:52 +0200

    Revert "feat: create product-service"
    This reverts commit 7e0ef4c7

--- packages/client/src/components/pages/PageProducts/components/Products.tsx
+++ packages/client/src/components/pages/PageProducts/components/Products.tsx
@@ -9,8 +9,8 @@ import {makeStyles} from '@material-ui/core/styles';
 import {Product} from "models/Product";
 import {formatAsPrice} from "utils/utils";
 import AddProductToCart from "components/AddProductToCart/AddProductToCart";
-import axios from 'axios';
-import API_PATHS from "constants/apiPaths";
+// import axios from 'axios';
+// import API_PATHS from "constants/apiPaths";
 import productList from "./productList.json";
 
 const useStyles = makeStyles((theme) => ({
@@ -36,11 +36,10 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    axios
-      .get(`${API_PATHS.bff}/products`)
-      .then((res) => setProducts(res.data));
+    // axios.get(`${API_PATHS.bff}/product/available/`)
+    //   .then(res => setProducts(res.data));
     setProducts(productList);
-  }, []);
+  }, [])
 
   return (
     <Grid container spacing={4}>
--- packages/product-service/cors.ts
+++
@@ -1,4 +0,0 @@
-export const cors = {
-  'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-  'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-};
