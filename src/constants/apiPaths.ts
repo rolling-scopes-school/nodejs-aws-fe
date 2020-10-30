a2bc33293b106ecf2ddebdfce25adcd20bd1aece
Чернышенко Михаил Дмитриевич <Mikhail_Chernyshenko@epam.com>
2020-10-30 21:01:26 +0300

    feat: request products from labda BE

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,8 @@
-
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://pp6vr2duqa.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
