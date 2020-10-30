9a551c4bd2ffdc664d03a9f2ec009f6730a44fc5
Evgeny Romanov (EPAM) <evgeny_romanov@epam.com>
2020-10-30 16:13:29 +0100

    NodeJS in AWS: Task 3: First API with AWS API Gateway & AWS Lambda Removed hardcoded productList.json and switched to calls to backend.

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -9,9 +9,8 @@ import {makeStyles} from '@material-ui/core/styles';
 import {Product} from "models/Product";
 import {formatAsPrice} from "utils/utils";
 import AddProductToCart from "components/AddProductToCart/AddProductToCart";
-// import axios from 'axios';
-// import API_PATHS from "constants/apiPaths";
-import productList from "./productList.json";
+import axios from 'axios';
+import API_PATHS from "constants/apiPaths";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -36,9 +35,8 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    // axios.get(`${API_PATHS.bff}/product/available/`)
-    //   .then(res => setProducts(res.data));
-    setProducts(productList);
+    axios.get(`${API_PATHS.bff}/products`)
+      .then(res => setProducts(res.data));
   }, [])
 
   return (
