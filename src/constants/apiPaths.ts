fdf43b07e5559bd62ad3961e537199140fc3795f
dramaniuk <dramaniuk@exadel.com>
2020-10-31 12:37:06 +0300

    change api_paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://hhs4y24seg.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://hhs4y24seg.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
