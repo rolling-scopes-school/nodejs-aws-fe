abbf9019ffc343b4598b92baeab7ace8b8e1db7a
maximsan <maximuzz@yandex.ru>
2020-10-31 19:53:39 +0300

    feature: remove productList.json, add getProductList lambda call

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -9,62 +9,69 @@ import {makeStyles} from '@material-ui/core/styles';
 import {Product} from "models/Product";
 import {formatAsPrice} from "utils/utils";
 import AddProductToCart from "components/AddProductToCart/AddProductToCart";
-// import axios from 'axios';
-// import API_PATHS from "constants/apiPaths";
-import productList from "./productList.json";
+import axios from 'axios';
+import API_PATHS from "constants/apiPaths";
 
 const useStyles = makeStyles((theme) => ({
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
 }));
 
 export default function Products() {
-  const classes = useStyles();
-  const [products, setProducts] = useState<Product[]>([]);
+    const classes = useStyles();
+    const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    // axios.get(`${API_PATHS.bff}/product/available/`)
-    //   .then(res => setProducts(res.data));
-    setProducts(productList);
-  }, [])
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const {data} = await axios.get(`${API_PATHS.product}/products`);
+                setProducts(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
 
-  return (
-    <Grid container spacing={4}>
-      {products.map((product: Product, index: number) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.cardMedia}
-              image={`https://source.unsplash.com/collection/9828738?sig=${index}`}
-              title="Image title"
-            />
-            <CardContent className={classes.cardContent}>
-              <Typography gutterBottom variant="h5" component="h2">
-                {product.title}
-              </Typography>
-              <Typography>
-                {formatAsPrice(product.price)}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <AddProductToCart product={product}/>
-            </CardActions>
-          </Card>
+        getProducts().catch(console.error);
+    }, [])
+
+    return (
+        <Grid container spacing={4}>
+            {products.map((product: Product, index: number) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4}>
+                    <Card className={classes.card}>
+                        <CardMedia
+                            className={classes.cardMedia}
+                            image={`https://source.unsplash.com/collection/9828738?sig=${index}`}
+                            title="Image title"
+                        />
+                        <CardContent className={classes.cardContent}>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {product.title}
+                            </Typography>
+                            <Typography gutterBottom variant="body1">{product.description}</Typography>
+                            <Typography>
+                                {formatAsPrice(product.price)}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <AddProductToCart product={product}/>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            ))}
         </Grid>
-      ))}
-    </Grid>
-  );
+    );
 }
