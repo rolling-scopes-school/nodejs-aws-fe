183133b0ef9f618ea79e68ae4a3df4ca528ab33f
Maksim Murzin <maksim_murzin@epam.com>
2020-10-31 22:45:29 +0300

    Integrate with back-end

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://ej9c12cgfl.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://ej9c12cgfl.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ej9c12cgfl.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://ej9c12cgfl.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
