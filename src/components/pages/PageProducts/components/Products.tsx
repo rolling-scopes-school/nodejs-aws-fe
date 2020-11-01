cac2a437823652dc08cfe1b1693d939f0edefbf7
Svetlana Beliaeva <svetlana_beliaeva@epam.com>
2020-11-01 12:26:50 +0300

    Change image source

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -49,7 +49,7 @@ export default function Products() {
           <Card className={classes.card}>
             <CardMedia
               className={classes.cardMedia}
-              image={`https://source.unsplash.com/random?sig=${index}`}
+              image={product.imageUrl}
               title="Image title"
             />
             <CardContent className={classes.cardContent}>
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,6 +5,7 @@ export type Product = {
   title: string,
   description: string,
   price: number,
+  imageUrl: string,
 };
 
 export const ProductSchema = Yup.object().shape({
