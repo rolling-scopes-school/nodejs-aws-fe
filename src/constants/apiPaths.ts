573f214974b75a99afa458b85b9c2ff7c4df44f6
andech <7578191@gmail.com>
2020-11-01 21:47:31 +0300

    Added endpoint usage for getting products from BE

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
-
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  products:
+    'https://a5wex8b3kd.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
