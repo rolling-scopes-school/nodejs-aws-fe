1f0013f7567bc3fb0456a9ef4c2ff3a4b5a69161
Oleksandr Halahan <Oleksandr_Halahan@epam.com>
2020-11-01 22:55:02 +0200

    Get products through AWS API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev/products',
+  order: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev/products',
+  import: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev/products',
+  bff: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev/products'
 };
 
 export default API_PATHS;
