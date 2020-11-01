49aee55ba9e0056871efa622f0676a32bf0aad7f
Oleksandr Halahan <Oleksandr_Halahan@epam.com>
2020-11-01 22:57:49 +0200

    fix API path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev/products',
-  order: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev/products',
-  import: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev/products',
-  bff: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev/products'
+  product: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://gant9q18wd.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
