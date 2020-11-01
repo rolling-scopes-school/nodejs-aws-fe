3147355b82a0a7a150b2aa0fd57fd2c48533379b
Michael Markeev <michael.markeev@rokolabs.com>
2020-11-01 13:12:00 +0400

    done

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://jj5ok3shmg.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://jj5ok3shmg.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://jj5ok3shmg.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://jj5ok3shmg.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
