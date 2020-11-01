9a2cf8ab8e69535bdf21ee39668bf14e42a8e984
Denis Shiriaev <denis.shiriaev@novartis.com>
2020-11-01 21:17:53 +0300

    feat: Connect to Backend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://8hsosw75y7.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://8hsosw75y7.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://8hsosw75y7.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://8hsosw75y7.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
