447c6a3d04c3b3cd0cf4995b8dad2605e8e134c2
ihargit <ihar.kharashun@gmail.com>
2020-11-01 23:30:46 +0300

    chore: point bff to prod product service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://codrlwgu06.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://codrlwgu06.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://b2yymidh8l.execute-api.eu-west-1.amazonaws.com/prod/'
 };
 
 export default API_PATHS;
