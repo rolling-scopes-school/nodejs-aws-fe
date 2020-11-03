fedcf475092016f6805fa7f3cff83a31d33e83f9
Уматай Нургиса <nurgissa_umatay@epam.com>
2020-11-03 18:49:09 +0600

    fix: endpoints that use products suffix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
   product: 'https://g7wiw0v6af.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://g7wiw0v6af.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://g7wiw0v6af.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://g7wiw0v6af.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
