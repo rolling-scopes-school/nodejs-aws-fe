8b738f194f0f77e222bf1a988591f8f7a5d9a7a5
Уматай Нургиса <nurgissa_umatay@epam.com>
2020-11-03 22:02:43 +0600

    fix: Update new aws endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://g7wiw0v6af.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://g7wiw0v6af.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://g7wiw0v6af.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://g7wiw0v6af.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://p6cq8jgiid.execute-api.eu-west-1.amazonaws.com',
+  order: 'https://p6cq8jgiid.execute-api.eu-west-1.amazonaws.com',
+  import: 'https://p6cq8jgiid.execute-api.eu-west-1.amazonaws.com',
+  bff: 'https://p6cq8jgiid.execute-api.eu-west-1.amazonaws.com'
 };
 
 export default API_PATHS;
