32619ae522b26819af656fea279cddb2720bbd14
MariiaTarasiuk <72539820+MariiaTarasiuk@users.noreply.github.com>
2020-11-05 22:52:33 +0200

    feat: task 3 (#2)
    * add product api
    
    * fix for productList update
    
    Co-authored-by: Mariia Tarasiuk <mariia.tarasiuk.1@gmail.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,8 @@
-
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  product: "https://0xyi26x4qh.execute-api.eu-west-1.amazonaws.com/dev/products",
+  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
