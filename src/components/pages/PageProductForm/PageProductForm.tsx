40e4460b23dc6c299370ba186f90b83671e8fccb
Volodymyr Skoryk <volodymyr_skorik@gmail.com>
2020-11-07 15:08:58 +0200

    task 4

--- src/components/pages/PageProductForm/PageProductForm.tsx
+++ src/components/pages/PageProductForm/PageProductForm.tsx
@@ -109,8 +109,14 @@ export default function PageProductForm() {
   const onSubmit = (values: FormikValues) => {
     const formattedValues = ProductSchema.cast(values);
     const productToSave = id ? {...ProductSchema.cast(formattedValues), id} : formattedValues;
-    axios.put(`${API_PATHS.bff}/product`, productToSave)
-      .then(() => history.push('/admin/products'));
+    if(id) {
+      axios.put(`${API_PATHS.bff}/product`, productToSave)
+        .then(() => history.push('/admin/products'));
+    }
+    else {
+      axios.post(`${API_PATHS.bff}/products`, productToSave)
+        .then(() => history.push('/admin/products'))
+    }
   };
 
   useEffect(() => {
