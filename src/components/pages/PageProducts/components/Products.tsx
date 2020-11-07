28bc4e7fccf5bb35ffa8899cfc7196cc4ea11307
Valentin Grinkevich <grinkevich.vv@gmail.com>
2020-11-07 16:42:52 +0300

    fix other endpoints

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -35,7 +35,7 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    axios.get(`${API_PATHS.product}/products/`)
+    axios.get(`${API_PATHS.bff}/product`)
       .then(res => setProducts(res.data))
       .catch((e)=>console.error(e));
   }, [])
