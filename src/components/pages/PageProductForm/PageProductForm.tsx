81b348e506d51a907fe8dd50f0b2e13f526e00bc
maximsan <maximuzz@yandex.ru>
2020-11-08 23:12:25 +0300

    fix: change urls, put method in post for products

--- src/components/pages/PageProductForm/PageProductForm.tsx
+++ src/components/pages/PageProductForm/PageProductForm.tsx
@@ -1,153 +1,156 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { Product, ProductSchema } from 'models/Product';
-import { Formik, Field, FormikProps, FormikValues } from 'formik';
-import { TextField } from 'formik-material-ui';
+import {Product, ProductSchema} from 'models/Product';
+import {Formik, Field, FormikProps, FormikValues} from 'formik';
+import {TextField} from 'formik-material-ui';
 import axios from 'axios';
-import { useHistory, useParams } from 'react-router-dom';
+import {useHistory, useParams} from 'react-router-dom';
 import PaperLayout from 'components/PaperLayout/PaperLayout';
 import Typography from '@material-ui/core/Typography';
 import API_PATHS from 'constants/apiPaths';
-import { Spinner } from 'components/Spinner/Spinner';
+import {Spinner} from 'components/Spinner/Spinner';
 
 const Form = (props: FormikProps<FormikValues>) => {
-  const {
-    // values,
-    // touched,
-    // errors,
-    dirty,
-    isSubmitting,
-    isValid,
-    // handleChange,
-    // handleBlur,
-    handleSubmit,
-    // handleReset,
-    // setFieldValue,
-    // isEditMode,
-    // onCancel,
-    // isButtonContact,
-    // setTouched,
-    // isButtonAddAndRedirect,
-    // setShouldRedirect,
-    // submitForm,
-    // onGetCitizen,
-    // shouldConfirmLeave,
-  } = props;
+    const {
+        // values,
+        // touched,
+        // errors,
+        dirty,
+        isSubmitting,
+        isValid,
+        // handleChange,
+        // handleBlur,
+        handleSubmit,
+        // handleReset,
+        // setFieldValue,
+        // isEditMode,
+        // onCancel,
+        // isButtonContact,
+        // setTouched,
+        // isButtonAddAndRedirect,
+        // setShouldRedirect,
+        // submitForm,
+        // onGetCitizen,
+        // shouldConfirmLeave,
+    } = props;
 
-  return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Field
-            component={TextField}
-            name="title"
-            label="Title"
-            fullWidth
-            autoComplete="off"
-            required
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Field
-            component={TextField}
-            name="description"
-            label="Description"
-            fullWidth
-            autoComplete="off"
-            multiline
-            required
-          />
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Field
-            component={TextField}
-            name="price"
-            label="Price ($)"
-            fullWidth
-            autoComplete="off"
-            required
-          />
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Field
-            component={TextField}
-            name="count"
-            label="Count"
-            fullWidth
-            autoComplete="off"
-            required
-          />
-        </Grid>
-        <Grid item container xs={12} justify="space-between">
-          <Button color="primary">Cancel</Button>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            disabled={!dirty || isSubmitting || !isValid}
-          >
-            Save Product
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit} autoComplete="off">
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Field
+                        component={TextField}
+                        name="title"
+                        label="Title"
+                        fullWidth
+                        autoComplete="off"
+                        required
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Field
+                        component={TextField}
+                        name="description"
+                        label="Description"
+                        fullWidth
+                        autoComplete="off"
+                        multiline
+                        required
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Field
+                        component={TextField}
+                        name="price"
+                        label="Price ($)"
+                        fullWidth
+                        autoComplete="off"
+                        required
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Field
+                        component={TextField}
+                        name="count"
+                        label="Count"
+                        fullWidth
+                        autoComplete="off"
+                        required
+                    />
+                </Grid>
+                <Grid item container xs={12} justify="space-between">
+                    <Button color="primary">Cancel</Button>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        disabled={!dirty || isSubmitting || !isValid}
+                    >
+                        Save Product
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>
+    );
 };
 
 const emptyValues: any = ProductSchema.cast();
 
 export default function PageProductForm() {
-  const history = useHistory();
-  const { id } = useParams();
-  const [product, setProduct] = useState<Product | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+    const history = useHistory();
+    const {id} = useParams();
+    const [product, setProduct] = useState<Product | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const onSubmit = (values: FormikValues) => {
-    const formattedValues = ProductSchema.cast(values);
-    const productToSave = id
-      ? { ...ProductSchema.cast(formattedValues), id }
-      : formattedValues;
-    axios
-      .put(`${API_PATHS.bff}/product`, productToSave)
-      .then(() => history.push('/admin/products'));
-  };
-
-  useEffect(() => {
-    if (!id) {
-      setIsLoading(false);
-      return;
-    }
-    const getProductById = async () => {
-      try {
-        const { data } = await axios.get(`${API_PATHS.bff}/products/${id}`);
-        setProduct(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error(error);
-        setIsLoading(false);
-      }
+    const onSubmit = (values: FormikValues) => {
+        const formattedValues = ProductSchema.cast(values);
+        const productToSave = id
+            ? {...ProductSchema.cast(formattedValues), id}
+            : formattedValues;
+        axios
+            .post(`${API_PATHS.bff}/products`, productToSave)
+            .then(() => {
+                history.push('/admin/products')
+            })
+            .catch(console.error);
     };
 
-    getProductById().catch(console.error);
-  }, [id]);
+    useEffect(() => {
+        if (!id) {
+            setIsLoading(false);
+            return;
+        }
+        const getProductById = async () => {
+            try {
+                const {data} = await axios.get(`${API_PATHS.bff}/products/${id}`);
+                setProduct(data);
+                setIsLoading(false);
+            } catch (error) {
+                console.error(error);
+                setIsLoading(false);
+            }
+        };
 
-  if (isLoading) {
-    return <Spinner />;
-  }
+        getProductById().catch(console.error);
+    }, [id]);
 
-  return (
-    <PaperLayout>
-      <Typography component="h1" variant="h4" align="center">
-        {id ? 'Edit product' : 'Create new product'}
-      </Typography>
-      <Formik
-        initialValues={product || emptyValues}
-        validationSchema={ProductSchema}
-        onSubmit={onSubmit}
-      >
-        {(props: FormikProps<FormikValues>) => <Form {...props} />}
-      </Formik>
-    </PaperLayout>
-  );
+    if (isLoading) {
+        return <Spinner/>;
+    }
+
+    return (
+        <PaperLayout>
+            <Typography component="h1" variant="h4" align="center">
+                {id ? 'Edit product' : 'Create new product'}
+            </Typography>
+            <Formik
+                initialValues={product || emptyValues}
+                validationSchema={ProductSchema}
+                onSubmit={onSubmit}
+            >
+                {(props: FormikProps<FormikValues>) => <Form {...props} />}
+            </Formik>
+        </PaperLayout>
+    );
 }
