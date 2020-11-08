a58897392ca1320a6d3c354b38f0fa59e0933b07
Viachaslau_karotki@epam.com <Viachaslau_karotki@epam.com>
2020-11-09 02:22:47 +0300

    Task4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
   product: 'https://lbxm5xv6d2.execute-api.eu-central-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  order: 'https://lbxm5xv6d2.execute-api.eu-central-1.amazonaws.com/dev',
+  import: 'https://lbxm5xv6d2.execute-api.eu-central-1.amazonaws.com/dev',
+  bff: 'https://lbxm5xv6d2.execute-api.eu-central-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
