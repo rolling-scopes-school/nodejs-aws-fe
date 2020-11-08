cc3289b5e56e1135b048a6f1776fce4d243e6fa0
Михаил Калита <mkalita1991@gmail.com>
2020-11-08 18:19:05 +0300

    fix: count is integer

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -13,5 +13,5 @@ export const ProductSchema = Yup.object().shape({
   title: Yup.string().required(),
   description: Yup.string(),
   price: Yup.number().required(),
-  count: Yup.number(),
+  count: Yup.number().integer(),
 });
