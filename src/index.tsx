2e4917e1a240d72e07496b0786c18b8cc05fcbac
Volodymyr Skoryk <volodymyr_skorik@gmail.com>
2020-11-10 09:31:39 +0200

    changed alert on error

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,8 @@ axios.interceptors.response.use(
   },
   function(error) {
     if (error.response.status === 400) {
-      alert(error.response.data?.data);
+      
+      alert(error.response.data?.message);
     }
     return Promise.reject(error.response);
   }
