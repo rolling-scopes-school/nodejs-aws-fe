f9dbe401cb9bc464737ba685d527320beb23cc08
ArtiFisher <artemis.fisher@gmail.com>
2020-11-11 19:13:04 +0300

    feat: Set API URL for importing

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://aqnw2royh7.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://g86uhrmyl7.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
