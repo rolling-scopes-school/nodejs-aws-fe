af8dbe3c4721c2443d0dc3b3169e6ef01e789178
Svetlana Beliaeva <svetlana_beliaeva@epam.com>
2020-11-11 18:46:54 +0300

    Fixes

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -36,7 +36,7 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    axios.get(`${API_PATHS.product}/products/`)
+    axios.get(`${API_PATHS.product}/products`)
       .then(res => {
         setProducts(res.data.products);
       })
