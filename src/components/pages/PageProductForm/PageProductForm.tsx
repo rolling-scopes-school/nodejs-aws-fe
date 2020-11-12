90e1a7e2d448d037d9766566e6e2c4fcd570dc4b
Konstantin Zharich <kostyasupikos@gmail.com>
2020-11-12 10:46:44 +0300

    [AWS-TASK-4] - Cloud databases
    - added fe side logic

--- src/components/pages/PageProductForm/PageProductForm.tsx
+++ src/components/pages/PageProductForm/PageProductForm.tsx
@@ -106,11 +106,11 @@ export default function PageProductForm() {
   const [product, setProduct] = useState<Product | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const onSubmit = (values: FormikValues) => {
+  const onSubmit = async (values: FormikValues) => {
     const formattedValues = ProductSchema.cast(values);
     const productToSave = id ? {...ProductSchema.cast(formattedValues), id} : formattedValues;
-    axios.put(`${API_PATHS.bff}/product`, productToSave)
-      .then(() => history.push('/admin/products'));
+    await axios.post(`${API_PATHS.products}`, productToSave);
+    history.push('/');
   };
 
   useEffect(() => {
