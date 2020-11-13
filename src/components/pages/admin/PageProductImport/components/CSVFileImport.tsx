16721cb7a01912909afe37d7f52b1b5b99029af4
Artsiom <artsiom_peshko@epam.com>
2020-11-14 01:07:59 +0300

    add fifth task

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -43,6 +43,9 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
       console.log('Uploading to: ', response.data)
       const result = await fetch(response.data, {
         method: 'PUT',
+        headers: {
+          'Content-Type': 'text/csv',
+        },
         body: file
       })
       console.log('Result: ', result)
