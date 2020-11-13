2407d71065e35d19af79c3a7da5fc6d5ed572d96
ianchuk <kirill.ianchuk@merck.com>
2020-11-13 17:41:22 +0300

    Task 5: Integrated import API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://4xh4vm4dbf.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://fnycl1qikh.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
