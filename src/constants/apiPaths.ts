8c79a0671908cab40712ed504b8f1505f042abfa
Volodymyr Skoryk <volodymyr_skorik@gmail.com>
2020-11-14 15:09:58 +0200

    import products path edited

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://asb3mmmcsa.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://jh16kdf75j.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
