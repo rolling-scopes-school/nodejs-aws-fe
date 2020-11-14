f688673e9081e283df0965d779217a865161b80b
ViktoryiaKlimenka <Viktoryia_Klimenka@epam.com>
2020-11-15 02:24:58 +0300

    fix(hw5): use proper import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://dnfpunajrc.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ou6esb7r25.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://dnfpunajrc.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
