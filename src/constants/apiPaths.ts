77da9c2de1f0aee793c5479bd88096e50c3d0bf4
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-11-14 11:09:20 +0100

    refactor: change import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,7 @@
-
 const API_PATHS = {
   product: 'https://6ensb26b5m.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://6ensb26b5m.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://6ensb26b5m.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://4i2lncwk5k.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://6ensb26b5m.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
