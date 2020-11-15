d340d0b3eb759a86795b8f892ffcd039971f9931
Pavel_Uvarov <pavel_uvarov@epam.com>
2020-11-15 18:22:58 +0300

    fix image url

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -49,7 +49,7 @@ export default function Products() {
           <Card className={classes.card}>
             <CardMedia
               className={classes.cardMedia}
-              image={product.imageUrl}
+              image={product.imageurl}
               title="Image title"
             />
             <CardContent className={classes.cardContent}>
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,7 +5,7 @@ export type Product = {
   title: string,
   description: string,
   price: number,
-  imageUrl: string 
+  imageurl: string 
 };
 
 export const ProductSchema = Yup.object().shape({
