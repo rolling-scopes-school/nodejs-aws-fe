4f78912f8983c1525bf044dc5c2911ead88128a2
Oleksandr Halahan <Oleksandr_Halahan@epam.com>
2020-11-15 02:09:23 +0200

    change import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://m4vi868ne8.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
