22072ce700bc2462346ee4b118feb098319bdbfc
Galina Kasatkina <Galina_Kasatkina@epam.com>
2020-11-15 13:13:06 +0100

    Second Task: Serve SPA in AWS (S3, Cloudfront, Serverless)

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -8,7 +8,7 @@ import Header from "components/MainLayout/components/Header";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {'Copyright © 2020'}
       <Link color="inherit" href="https://material-ui.com/">
         My Store
       </Link>{' '}
