54cfff3cb6e28e91435a2c712aa311f69d15077f
slava <vyacheslav.karnauh@gmail.com>
2020-11-15 11:36:40 +0200

    update path for products import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://julbe5p894.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://cc3mwqo2y1.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
