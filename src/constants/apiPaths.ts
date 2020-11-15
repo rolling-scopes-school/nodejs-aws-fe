8dcdead9f2cfc0c370da94f3840ef6694495ef9f
Aleksandra Sturova <Aleksandra_Sturova@epam.com>
2020-11-15 18:57:10 +0300

    feat: change import url for task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://l2xwwcjprk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://y8cv6h0sql.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
