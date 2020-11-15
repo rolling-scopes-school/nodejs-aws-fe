597b355d02ee6a574f42701bb6900a8cf4bb32bd
Yakov Grischenko <coding@mail.ru>
2020-11-15 16:04:44 +0700

    fix: new import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://f9cbmg19ad.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://yojudv04z2.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://jyzag4006l.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://f9cbmg19ad.execute-api.eu-west-1.amazonaws.com/dev',
 }
 
