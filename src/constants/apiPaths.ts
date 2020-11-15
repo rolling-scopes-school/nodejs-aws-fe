2ec8fef930b37e09b77c701665c7fe2c141b9606
Aleksandra Sturova <Aleksandra_Sturova@epam.com>
2020-11-15 18:19:55 +0300

    Task 3 | First API with AWS API Gateway, AWS Lambda (#2)
    * feat: add api endpoint for products
    
    * feat: add description to product card

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://y8cv6h0sql.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
