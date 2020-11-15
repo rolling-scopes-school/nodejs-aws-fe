cc92e937da1112dd29630e2f5fdd419ce1fa9ef0
Serhii_Temchenko <serhii_temchenko@epam.com>
2020-11-15 18:49:05 +0200

    Task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://bgwmagbr07.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://9aj51mlxlc.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
