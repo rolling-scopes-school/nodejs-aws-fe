6d41ff8ac29b1a4871d547163ff69b57ac0c1a1d
Ihar Amelyaniuk <igoromelyaniuk@gmail.com>
2020-11-15 12:11:46 +0300

    Sync with new import endpoint

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -41,7 +41,7 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
       })
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
-      const result = await fetch(response.data, {
+      const result = await fetch(response.data.url, {
         method: 'PUT',
         body: file
       })
