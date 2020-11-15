04bf42481402be1f88ca5355b35844204140d19a
ALeksei Osin <aleksei_osin@epam.com>
2020-11-16 01:00:46 +0400

    task5: integrate with import-service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ou3vz8mib5.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://r9tp5o4mh9.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ou3vz8mib5.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
