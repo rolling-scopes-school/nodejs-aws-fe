11176ea98780633e714e327cdcb02dc2114a44b4
Mariia Tarasiuk <mariia.tarasiuk.1@gmail.com>
2020-11-15 21:58:49 +0200

    update import api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://0xyi26x4qh.execute-api.eu-west-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://g8n64lq9sd.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://0xyi26x4qh.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
