7d64122ec54b1e47b21549538fa922a9b125116b
Evgeny Romanov (EPAM) <evgeny_romanov@epam.com>
2020-11-15 16:02:23 +0100

    NodeJS in AWS: Task 5: AWS Simple Storage Service - S3 CSV files import added

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://5h80lgbr6l.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://299bl3bv5g.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
