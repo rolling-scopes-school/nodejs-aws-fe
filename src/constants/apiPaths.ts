6f1c6a223c08c5b63f7df16a0fc17b4f19e1f8e4
Sevastsyan <em3rkof@gmail.com>
2020-11-16 20:13:27 +0300

    [refactor] apiPaths updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://z8c40bnfmi.execute-api.eu-west-1.amazonaws.com/dev/products',
+  product: 'https://z8c40bnfmi.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://vapcj1zvui.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
