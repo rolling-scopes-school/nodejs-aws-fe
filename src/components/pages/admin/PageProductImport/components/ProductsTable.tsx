cce5b6d47c304d972d816a85e6f811e97eaf3135
Maksym Kaspriv <vestry.odessa@gmail.com>
2020-11-17 00:27:58 +0200

    fix: add letter s in products path

--- src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
+++ src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
@@ -21,9 +21,9 @@ export default function ProductsTable() {
   }, []);
 
   const onDelete = (id: string) => {
-    axios.delete(`${API_PATHS.bff}/product/${id}`)
+    axios.delete(`${API_PATHS.bff}/products/${id}`)
       .then(() => {
-        axios.get(`${API_PATHS.bff}/product`)
+        axios.get(`${API_PATHS.bff}/products`)
           .then(res => setProducts(res.data));
         }
       );
