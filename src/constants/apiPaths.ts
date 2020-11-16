873089e8f74e71b9b72066b46515229cb66e95a3
Maksym Kaspriv <vestry.odessa@gmail.com>
2020-11-16 23:32:29 +0200

    feat: add path to import files in s3 bucket

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
   product: 'https://l9ao1q7lel.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://l9ao1q7lel.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://l9ao1q7lel.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://hjsa9wtf1j.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://hjsa9wtf1j.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://l9ao1q7lel.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
