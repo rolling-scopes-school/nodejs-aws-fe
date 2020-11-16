88f55f1b0c72537e321000abd41805c59d84669e
Andrey Postylyakov <andrey_postylyakov@epam.com>
2020-11-16 16:33:59 +0300

    task-05 (https://github.com/rolling-scopes-school/nodejs-aws-tasks/blob/main/task5-import-to-s3/task.md) updated api endpoint and content-type for the import-service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ydrurck6qi.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://ydrurck6qi.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://ydrurck6qi.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ryygla05ak.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ydrurck6qi.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
