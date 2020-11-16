03e3a18787482fe68aea4ac66bf47ae2859a4da5
Stanislau Berazouski <stanislau_berazouski@epam.com>
2020-11-17 01:17:39 +0300

    [task 5] change request url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://9smeldy59c.execute-api.eu-west-1.amazonaws.com/dev/',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://x362f8qb4j.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
