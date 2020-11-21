08e97c5c0282ddd6e60acca63ae5ea6daeaf8027
Volodymyr Skoryk <volodymyr_skorik@gmail.com>
2020-11-21 17:09:37 +0200

    updated api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://jh16kdf75j.execute-api.eu-west-1.amazonaws.com/dev'
+  import: 'https://qywkt2ds6g.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://5qpce4wdik.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
