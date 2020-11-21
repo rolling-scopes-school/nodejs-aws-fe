e920b6dcce6c09778db3c4eeff6ffb1dfda6d03a
Aleksandr Lukovnikov <Aleksandr_Lukovnikov@epam.com>
2020-11-21 16:17:52 +0300

    task 5 - update FE from rsschool

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -18,22 +18,12 @@ type CSVFileImportProps = {
 export default function CSVFileImport({url, title}: CSVFileImportProps) {
   const classes = useStyles();
   const [file, setFile] = useState<any>();
-  const [uploadUrl, setUploadUrl] = useState<any>();
-
-  const createFile = (file: any) => {
-    let reader = new FileReader()
-    reader.onload = (e: any) => {
-      console.log(e.target.result);
-      setFile(e.target.result);
-    }
-    reader.readAsDataURL(file)
-  };
 
   const onFileChange = (e: any) => {
     console.log(e);
     let files = e.target.files || e.dataTransfer.files
     if (!files.length) return
-    createFile(files[0])
+    setFile(files.item(0));
   };
 
   const removeFile = () => {
@@ -42,26 +32,23 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async (e: any) => {
       // Get the presigned URL
-      const response = await axios({
+      const {data} = await axios({
         method: 'GET',
-        url
+        url,
+        params: {
+          name: encodeURIComponent(file.name)
+        }
       })
-      console.log('Response: ', response.data)
-      console.log('Uploading: ', file)
-      let binary = atob(file.split(',')[1])
-      let array = []
-      for (var i = 0; i < binary.length; i++) {
-        array.push(binary.charCodeAt(i))
-      }
-      let blobData = new Blob([new Uint8Array(array)], {type: 'text/plain'})
-      console.log('Uploading to: ', response.data.uploadURL)
-      const result = await fetch(response.data.uploadURL, {
+      console.log('File to upload: ', file.name)
+      console.log('Uploading to: ', data)
+      const result = await fetch(data, {
         method: 'PUT',
-        body: blobData
+        body: file,
+        headers: {
+          'Content-Type': 'text/csv'
+        }
       })
       console.log('Result: ', result)
-      // Final URL for the user doesn't need the query string params
-      setUploadUrl(response.data.uploadURL.split('?')[0]);
       setFile('');
     }
   ;
@@ -75,8 +62,8 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
           <input type="file" onChange={onFileChange}/>
       ) : (
         <div>
-          {!uploadUrl && <button onClick={removeFile}>Remove file</button>}
-          {!uploadUrl && <button onClick={uploadFile}>Upload file</button>}
+          <button onClick={removeFile}>Remove file</button>
+          <button onClick={uploadFile}>Upload file</button>
         </div>
       )}
     </div>
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,11 +1,14 @@
-const API_PREFIX = 'https://rkn3w31r6f.execute-api.eu-west-1.amazonaws.com/dev';
+const API_PREFIX_PRODUCT = 'https://rkn3w31r6f.execute-api.eu-west-1.amazonaws.com/dev';
+const API_PREFIX_IMPORT = 'https://0oronyurq7.execute-api.eu-west-1.amazonaws.com/dev';
 
 const API_PATHS = {
-  products: `${API_PREFIX}/products`,
-  create: `${API_PREFIX}/product`,
-  order: `${API_PREFIX}/order`,
-  import: `${API_PREFIX}/import`,
-  bff: `${API_PREFIX}/api/v1`,
+  products: `${API_PREFIX_PRODUCT}/products`,
+  create: `${API_PREFIX_PRODUCT}/product`,
+  order: `${API_PREFIX_PRODUCT}/order`,
+
+  import: `${API_PREFIX_IMPORT}/import`,
+
+  bff: `${API_PREFIX_PRODUCT}/api/v1`,
 };
 
 export default API_PATHS;
