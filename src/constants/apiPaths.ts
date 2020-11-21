41a7585446b55d51cc2fcc01339a7c472bf59c5c
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2020-11-21 21:13:43 +0300

    update api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  products: 'https://2s5xprqt4k.execute-api.eu-west-1.amazonaws.com/dev/products',
+  products: 'https://hrxry5b1t8.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://mmd5l7u8kl.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://2s5xprqt4k.execute-api.eu-west-1.amazonaws.com/dev/'
+  bff: 'https://hrxry5b1t8.execute-api.eu-west-1.amazonaws.com/dev/'
 };
 
 export default API_PATHS;
