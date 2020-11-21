c5e9ecfcabdb009c6f8c298393cef41f665f4b02
Andrei Sianiuta <anderysenuta@gmail.com>
2020-11-21 16:45:16 +0300

    change path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
   product: 'https://hls6p04919.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://hls6p04919.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://a80ff3jkpj.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://hls6p04919.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
