84f02eb66d0f786f27862e4d3ffd1bc568480585
andech <7578191@gmail.com>
2020-11-23 08:58:36 +0300

    Changed URL for API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   products:
-    'https://a5wex8b3kd.execute-api.eu-west-1.amazonaws.com/dev/products',
+    'https://hg910sjfw7.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://idbcvtwhbl.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://a5wex8b3kd.execute-api.eu-west-1.amazonaws.com/dev/products',
+  bff: 'https://hg910sjfw7.execute-api.eu-west-1.amazonaws.com/dev/products',
 };
 
 export default API_PATHS;
