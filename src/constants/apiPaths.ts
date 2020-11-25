baad435de7a9bde6604f86d42a8e149c5b136ecb
Maksym Kaspriv <vestry.odessa@gmail.com>
2020-11-25 23:50:20 +0200

    refactor: change api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://l9ao1q7lel.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://hjsa9wtf1j.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://hjsa9wtf1j.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://l9ao1q7lel.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://raefy982k8.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://bwqmg9w4o1.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://bwqmg9w4o1.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://raefy982k8.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
