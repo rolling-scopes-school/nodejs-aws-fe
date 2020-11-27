e1693c3774ef73770fd1c4f3def493d73ee2e081
Gennadiixd <gennadiixd@gmail.com>
2020-11-27 21:49:51 +0300

    add import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://be2sq45tbl.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://be2sq45tbl.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://be2sq45tbl.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://lqqrsfz7s6.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://be2sq45tbl.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
