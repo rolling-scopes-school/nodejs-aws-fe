f1fe19cafad49ce66d817fba0574aa2903bf4b74
kotmatpockuh <dobrenko92@gmail.com>
2020-11-27 12:31:52 +0300

    feat: task-5 fix import api path (#5)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
     product: 'https://c317st8p5i.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://2uvvqem7lk.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
