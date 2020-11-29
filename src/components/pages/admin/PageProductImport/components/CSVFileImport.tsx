5022961707f904c6b82867977e0faece706a7c6c
ihargit <ihar.kharashun@gmail.com>
2020-11-29 22:46:33 +0300

    feat: add authorization for csv import

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -33,12 +33,17 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async (e: any) => {
       // Get the presigned URL
+      const authorizationToken = localStorage.getItem('authorization_token');
       const response = await axios({
         method: 'GET',
         url,
         params: {
           name: encodeURIComponent(file.name)
+        },
+        headers: {
+          Authorization: `Bearer ${authorizationToken}`
         }
+
       })
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
