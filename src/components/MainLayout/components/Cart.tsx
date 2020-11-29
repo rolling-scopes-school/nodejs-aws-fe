f739cc8e563e99ec838766d7c851973b5c90ddd5
Herman_Bilous <Herman_Bilous@epam.com>
2020-11-29 23:00:38 +0200

    Add cart api integration

--- src/components/MainLayout/components/Cart.tsx
+++ src/components/MainLayout/components/Cart.tsx
@@ -1,20 +1,35 @@
 import Badge from "@material-ui/core/Badge";
 import CartIcon from "@material-ui/icons/ShoppingCart";
 import IconButton from "@material-ui/core/IconButton";
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectCartItems } from "store/cartSlice";
+import React, { useEffect } from "react";
+import axios from 'axios';
+import { useDispatch, useSelector } from "react-redux";
+import { selectCartItems, updateFromApi } from "store/cartSlice";
 import { Link } from 'react-router-dom';
+import API_PATHS from "../../../constants/apiPaths";
 
 export default function Cart() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios.get(
+        `${API_PATHS.cart}/profile/cart`,
+        {
+          headers: {
+            Authorization: `Basic ${localStorage.getItem('authorization_token')}`
+          }
+        }
+    ).then(({ data: { data: { cart } } }) => {
+      dispatch(updateFromApi(cart))
+    });
+  }, []);
   const cartItems = useSelector(selectCartItems);
   const badgeContent = cartItems.length || undefined;
 
   return (
-    <IconButton 
-      aria-label="show 4 new mails" 
-      color="inherit" 
-      component={Link} 
+    <IconButton
+      aria-label="show 4 new mails"
+      color="inherit"
+      component={Link}
       to="/cart"
     >
       <Badge badgeContent={badgeContent} color="secondary">
