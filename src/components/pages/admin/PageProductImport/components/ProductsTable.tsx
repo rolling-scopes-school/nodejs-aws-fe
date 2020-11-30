acc001b2d9febe7d3dbb905487064560d35f3003
Andrei Sianiuta <anderysenuta@gmail.com>
2020-11-30 03:08:40 +0300

    TASK 7 add authentication header

--- src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
+++ src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
@@ -16,8 +16,8 @@ export default function ProductsTable() {
   const [products, setProducts] = useState<any>([]);
 
   useEffect(() => {
-    axios.get(`${API_PATHS.bff}/product`)
-      .then(res => setProducts(res.data));
+    // axios.get(`${API_PATHS.bff}/product`)
+    //   .then(res => setProducts(res.data));
   }, []);
 
   const onDelete = (id: string) => {
