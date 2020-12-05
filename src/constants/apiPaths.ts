e708ce6e406e0c687f894636195bffb0e20fbd49
Pavel_Uvarov <pavel_uvarov@epam.com>
2020-12-05 23:34:27 +0300

    add cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ttd9jvwxpd.execute-api.eu-west-1.amazonaws.com/dev',  
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://spiderpoul-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
