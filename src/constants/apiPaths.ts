0f600f92fa34e19b6de3ab34f0552cb5fdcb971a
Volodymyr Skoryk <volodymyr_skorik@gmail.com>
2020-12-05 14:39:39 +0200

    allowed http + changed cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://qywkt2ds6g.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://5qpce4wdik.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://skorykv-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 
