5b4734af71cfdd809bd9c9c5c4acf96b6e8c91db
Artsiom <artsiom_peshko@epam.com>
2020-12-05 14:52:25 +0300

    Task 8 (Docker. AWS Elastic Beanstalk)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://k8qj2nqrv7.execute-api.eu-west-1.amazonaws.com/dev/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://peshkoartembsu-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
