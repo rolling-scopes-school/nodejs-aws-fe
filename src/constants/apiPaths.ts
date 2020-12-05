f08190207accd89d899125551c8e605b4aa8448e
Aliaksei Hurynovich <shagon1k@gmail.com>
2020-12-05 20:51:22 +0300

    feat: updated cart api with new url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://dziq1ndu17.execute-api.eu-west-1.amazonaws.com/prod',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://shagon1k-carts-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
