e601a688d07507c3f5e99803ca786c3801a26ed7
Maksim Murzin <maksim_murzin@epam.com>
2020-12-06 13:06:33 +0300

    Change cart api on proxy address

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://ej9c12cgfl.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://7a3g13gk41.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ej9c12cgfl.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://mrdevice-cart-api.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://0i3rd7t8p1.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
