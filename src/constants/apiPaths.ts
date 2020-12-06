9810397a9e169afad88dd676b8bda2e9fa39fe95
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2020-12-06 14:06:59 +0300

    small modification

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   products: 'https://hrxry5b1t8.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://mmd5l7u8kl.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://hrxry5b1t8.execute-api.eu-west-1.amazonaws.com/dev/'
+  bff: 'https://hrxry5b1t8.execute-api.eu-west-1.amazonaws.com/dev/',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
