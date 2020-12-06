de55c39e6be0b143d1d4fe71cacb365dc4384a81
Oleksandr Halahan <oleksandr.halahan@gmail.com>
2020-12-06 20:23:02 +0200

    Integrate cart API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,11 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://m4vi868ne8.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://4wu2eb644j.execute-api.eu-west-1.amazonaws.com/dev',
+  // cart: 'http://ohalahan-cart-api-develop.eu-west-1.elasticbeanstalk.com/api', cannot used because it is served using HTTP
+  cart: 'https://no8p3hmfgh.execute-api.eu-west-1.amazonaws.com/develop/api', // use proxy instead
 };
 
 export default API_PATHS;
