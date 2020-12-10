34d3ee65b6a8c1175404d8b4da4f4b07bf403265
Alexander Kleshchukevich <keeweery+github@gmail.com>
2020-12-10 16:13:48 +0300

    add cart url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://egan0irg9a.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://5k5fgodiv3.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
