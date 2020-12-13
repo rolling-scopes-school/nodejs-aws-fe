d2cae7e645d03f720ea5a00f7c80e40003903fce
Ihar Amelyaniuk <igoromelyaniuk@gmail.com>
2020-12-13 22:07:22 +0300

    Task 9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: 'https://fs9nzzi704.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://btp28u4wfd.execute-api.eu-west-1.amazonaws.com/dev/product',
   order: 'https://fs9nzzi704.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://l36ht06j9k.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://fs9nzzi704.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://t7mozk1hd0.execute-api.eu-west-1.amazonaws.com/dev/api',
+  cart: 'https://btp28u4wfd.execute-api.eu-west-1.amazonaws.com/dev/cart',
 };
 
 export default API_PATHS;
