c8b1f75557ab17006d31295821ea32e2980f359f
aleksandr.kondratsk <aleksandr.kondratsk@ihsmarkit.com>
2021-03-12 10:37:28 +0400

    feat: task 1 - fixed defects, added thematic products, changed headers

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        Bells'n'Whistles Music Shop
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
