c79bfd1cb00bb00ff958c51479bc71bae89bd43a
Mariia_Guliaeva <mariia_guliaeva@epam.com>
2021-03-29 20:05:03 +0400

    integration with "getProductsList" lambda function in nodejs-aws-be project

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://.execute-api.us-east-1.amazonaws.com/dev',
+  order: 'https://.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://.execute-api.us-east-1.amazonaws.com/dev',
+  bff: 'https://bmyfe1s3e1.execute-api.us-east-1.amazonaws.com/dev',
+  cart: 'https://.execute-api.us-east-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
