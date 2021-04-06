793f84714922e074f26b296c6fbcebbe75f7d062
Leonid Petrov <LVPetrov@alfabank.ru>
2021-04-06 22:14:35 +0300

    feat: add link to be api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://f4aguhnp5h.execute-api.eu-west-1.amazonaws.com/dev/',
+  order: 'https://f4aguhnp5h.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://f4aguhnp5h.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://f4aguhnp5h.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://f4aguhnp5h.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
