9c9fc63a37bc06a4bddbc08b5a495bd5db41c2ac
Yuriy Tataryntsev <yuriy_tataryntsev@epam.com>
2021-04-07 19:14:31 +0300

    real products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
-
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  products: 'https://r1t72o85wg.execute-api.eu-west-1.amazonaws.com/dev/products',
+  product: 'https://r1t72o85wg.execute-api.eu-west-1.amazonaws.com/dev/product',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
@@ -8,3 +8,4 @@ const API_PATHS = {
 };
 
 export default API_PATHS;
+
