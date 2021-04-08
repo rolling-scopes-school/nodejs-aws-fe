e900705097490cce37bc7221bb1cefbdcd2a50e4
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-04-08 14:27:10 +0300

    improve api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://gp0go5x90f.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://gp0go5x90f.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://gp0go5x90f.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://gp0go5x90f.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://gp0go5x90f.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev/',
+  order: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
