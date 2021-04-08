32c91423f94f63475b45d1856c5e144aa880d024
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-04-08 14:42:55 +0300

    update paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
   product: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev/',
-  order: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
