865035719734100e2e21b4c796081259db561625
Ivan Piankov <Ivan_Piankov@epam.com>
2021-04-11 00:23:07 +0300

    task-3 using API endpoint for products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://7q7u27f90l.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
