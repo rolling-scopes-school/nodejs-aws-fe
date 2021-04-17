ca9363c5455bb8e241fe74004bfdc888b0c183b6
Anton Kuzmenko <anton_kuzmenko@epam.com>
2021-04-17 23:05:44 +0300

    Change deploy region

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://ihebawu540.execute-api.eu-central-1.amazonaws.com/prod',
+  product: 'https://xnj469am26.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://ihebawu540.execute-api.eu-central-1.amazonaws.com/prod',
+  bff: 'https://xnj469am26.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
