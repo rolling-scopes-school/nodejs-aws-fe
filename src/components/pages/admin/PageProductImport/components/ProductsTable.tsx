c8e6ea7fe29618be1a8edaa7ad4f5a6256152049
Yahor Sheinikau <sheynikovea@gmail.com>
2021-04-20 18:56:50 +0300

    feat: Fix the FE project to support product PUT request.

--- src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
+++ src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
@@ -1,34 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import API_PATHS from "constants/apiPaths";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import {formatAsPrice} from "utils/utils";
+import { formatAsPrice } from "utils/utils";
 
 export default function ProductsTable() {
   const [products, setProducts] = useState<any>([]);
 
   useEffect(() => {
-    axios.get(`${API_PATHS.bff}/product`)
-      .then(res => setProducts(res.data));
+    axios
+      .get(`${API_PATHS.bff}/products`)
+      .then((res) => res.data.products)
+      .then(setProducts);
   }, []);
 
   const onDelete = (id: string) => {
-    axios.delete(`${API_PATHS.bff}/product/${id}`)
-      .then(() => {
-        axios.get(`${API_PATHS.bff}/product`)
-          .then(res => setProducts(res.data));
-        }
-      );
+    axios.delete(`${API_PATHS.bff}/product/${id}`).then(() => {
+      axios
+        .get(`${API_PATHS.bff}/products`)
+        .then((res) => res.data.products)
+        .then(setProducts);
+    });
   };
-
 
   return (
     <TableContainer component={Paper}>
@@ -43,19 +44,30 @@ export default function ProductsTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {products.map((product: any) => (
+          {products?.map((product: any) => (
             <TableRow key={product.id}>
               <TableCell component="th" scope="row">
                 {product.title}
               </TableCell>
               <TableCell align="right">{product.description}</TableCell>
-              <TableCell align="right">{formatAsPrice(product.price)}</TableCell>
+              <TableCell align="right">
+                {formatAsPrice(product.price)}
+              </TableCell>
               <TableCell align="right">{product.count}</TableCell>
               <TableCell align="right">
-                <Button size="small" color="primary" component={Link} to={`/admin/product-form/${product.id}`}>
+                <Button
+                  size="small"
+                  color="primary"
+                  component={Link}
+                  to={`/admin/product-form/${product.id}`}
+                >
                   Manage
                 </Button>
-                <Button size="small" color="secondary" onClick={() => onDelete(product.id)}>
+                <Button
+                  size="small"
+                  color="secondary"
+                  onClick={() => onDelete(product.id)}
+                >
                   Delete
                 </Button>
               </TableCell>
