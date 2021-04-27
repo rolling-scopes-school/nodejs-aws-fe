b61b2a0e7e3b6e332bf08a413de878e68152190e
Aliaksei Lyzo <lyzo.alex@gmail.com>
2021-04-27 13:46:09 +0300

    Task5 - import CSV

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://8dt810m9yd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://tofwqej2ui.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
