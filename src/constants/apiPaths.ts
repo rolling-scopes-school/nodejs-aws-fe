9f0dcbea576d404d045f85656b1b1c420ad99262
akondratsky <akondratsky@yandex.ru>
2021-04-30 02:44:39 +0300

    integration of new service (upload file)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  products: 'https://2o4c9nhcai.execute-api.us-west-1.amazonaws.com/dev/products',
+  products: 'https://guasrpmoic.execute-api.us-east-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: ' https://2o4c9nhcai.execute-api.us-west-1.amazonaws.com/dev/import',
+  import: 'https://4zbmym8vyc.execute-api.us-east-1.amazonaws.com/dev/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
