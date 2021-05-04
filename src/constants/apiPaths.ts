facf736c3a77df42fc0f794fd7b4a747f8d13757
Maksym_Musiichuk <maksym.musiichuk@gmail.com>
2021-05-04 23:48:11 +0300

    EPAM-5: Change ApiPath

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -6,7 +6,7 @@ const API_PATHS = {
    */
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'http://localhost:3000/dev',
+  import: 'https://kj4o3xag9f.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
