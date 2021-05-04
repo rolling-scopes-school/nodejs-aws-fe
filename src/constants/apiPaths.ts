96bb96b7ba68f2cabace1ae16fa701fa8758dd78
Yahor Sheinikau <sheynikovea@gmail.com>
2021-05-04 15:21:53 +0300

    chore: Update apiPaths for the import service.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,12 +1,13 @@
-const apiHash = `uhvqepz8rb`;
+const productServiceHash = `uhvqepz8rb`;
+const importServiceHash = 'demqlfwkr8';
 
 
 const API_PATHS = {
-  product: `https://${apiHash}.execute-api.eu-west-1.amazonaws.com/dev`,
-  order: `https://${apiHash}.execute-api.eu-west-1.amazonaws.com/dev`,
-  import: `https://${apiHash}.execute-api.eu-west-1.amazonaws.com/dev`,
-  bff: `https://${apiHash}.execute-api.eu-west-1.amazonaws.com/dev`,
-  cart: `https://${apiHash}.execute-api.eu-west-1.amazonaws.com/dev`,
+  product: `https://${productServiceHash}.execute-api.eu-west-1.amazonaws.com/dev`,
+  order: `https://${null}.execute-api.eu-west-1.amazonaws.com/dev`,
+  import: `https://${importServiceHash}.execute-api.eu-west-1.amazonaws.com/dev`,
+  bff: `https://${null}.execute-api.eu-west-1.amazonaws.com/dev`,
+  cart: `https://${null}.execute-api.eu-west-1.amazonaws.com/dev`,
 };
 
 export default API_PATHS;
