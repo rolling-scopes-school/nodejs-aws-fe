0f6f0b30f2fa0234b1ca16fa0bcb4d526fff21fb
Maksym_Musiichuk <maksym.musiichuk@gmail.com>
2021-05-04 22:30:22 +0300

    EPAM-5: Integrating Uploading csv files

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'http://localhost:3000/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
