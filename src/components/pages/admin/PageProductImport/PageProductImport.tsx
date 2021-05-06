3ce4945fce9ffb839c63ba39666ebd927d8b2a0b
katya <weyulgex@gmail.com>
2021-05-06 19:27:59 +0300

    task-5

--- src/components/pages/admin/PageProductImport/PageProductImport.tsx
+++ src/components/pages/admin/PageProductImport/PageProductImport.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import API_PATHS from "constants/apiPaths";
-import ProductsTable from "components/pages/admin/PageProductImport/components/ProductsTable";
-import CSVFileImport from "components/pages/admin/PageProductImport/components/CSVFileImport";
-import Button from "@material-ui/core/Button";
-import Box from "@material-ui/core/Box";
-import {Link} from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import API_PATHS from 'constants/apiPaths';
+import ProductsTable from 'components/pages/admin/PageProductImport/components/ProductsTable';
+import CSVFileImport from 'components/pages/admin/PageProductImport/components/CSVFileImport';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   content: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(3, 0, 3),
@@ -20,12 +20,12 @@ export default function PageProductImport() {
   return (
     <div className={classes.content}>
       <Box display="flex" alignItems="center">
-        <CSVFileImport url={`${API_PATHS.import}/import`} title="Import Products CSV"/>
+        <CSVFileImport url={`${API_PATHS.import}/import`} title="Import Products CSV" />
         <Button size="small" color="primary" variant="contained" component={Link} to={'/admin/product-form/'}>
           create product
         </Button>
       </Box>
-      <ProductsTable/>
+      <ProductsTable />
     </div>
   );
 }
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   products: 'https://ln2y5dnph1.execute-api.eu-west-1.amazonaws.com/dev/products',
   product: 'https://ln2y5dnph1.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ox1rzqlxfb.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
