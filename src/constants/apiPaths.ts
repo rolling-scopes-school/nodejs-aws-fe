7e552545456e0f9841a597bef26bcdd65c898f2e
igor.shadrin_rft <F$~@r_9qWL>
2021-05-06 09:50:15 +0500

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://a5t41oeksj.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://a5t41oeksj.execute-api.us-east-1.amazonaws.com/dev',
-  import: 'https://a5t41oeksj.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://1y4lmwztj8.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://a5t41oeksj.execute-api.us-east-1.amazonaws.com/dev',
   cart: 'https://a5t41oeksj.execute-api.us-east-1.amazonaws.com/dev',
 };
