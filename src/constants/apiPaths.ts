59823f0d116719e1793d37a49cd8fb4f9913176a
Oleksiy Atamaniuk <alexata1204@gmail.com>
2021-05-07 20:41:58 +0300

    [TASK 5] Update path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@
 const API_PATHS = {
   product: 'https://ri5vzlll98.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://ri5vzlll98.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://zb6q6tsei7.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://ri5vzlll98.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
