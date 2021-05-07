84cd1f8ddc9730489f5ceca736d1985e3b05732f
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-05-07 16:47:57 +0300

    path for import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev/',
+  product: 'https://xcb4t8f702.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://vr8b888rvj.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
