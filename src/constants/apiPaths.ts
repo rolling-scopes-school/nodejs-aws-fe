90e1ac606205f429463c3f2b161737e8acded3aa
Mariia_Guliaeva <mariia_guliaeva@epam.com>
2021-05-17 11:29:01 +0400

    intergate with import service (importProductsFile function)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.us-east-1.amazonaws.com/dev',
-  import: 'https://.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://0pw9gt8mf5.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://wbnvs1sebc.execute-api.us-east-1.amazonaws.com/dev',
   cart: 'https://.execute-api.us-east-1.amazonaws.com/dev',
 };
