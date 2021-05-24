89b4606c519667abe6f5305eedaab119b485b6d7
Vitalii Puzakov <vitalii_puzakov@epam.com>
2021-05-24 16:40:39 +0300

    Changed API link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://94oxanc6jf.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://4zs4dvizz9.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://n8wgodtoe2.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://e9b7cgdp50.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
