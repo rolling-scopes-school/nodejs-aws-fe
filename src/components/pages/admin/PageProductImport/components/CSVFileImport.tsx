3e350c34896bf06f8b3d9e9804f883cc14908d3b
Maksym_Musiichuk <maksym.musiichuk@gmail.com>
2021-06-04 01:22:01 +0300

    FE PART

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -35,6 +35,9 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
       const response = await axios({
         method: 'GET',
         url,
+        headers: {
+          Authorization: localStorage.getItem('authorization_token'),
+        },
         params: {
           name: encodeURIComponent(file.name)
         }
