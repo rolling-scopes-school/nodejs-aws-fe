dc54cd29ed3237406e0f19b55a111d0ed2c873c6
Oleksiy Atamaniuk <alexata1204@gmail.com>
2021-06-03 10:47:58 +0300

    [AWS-TASK-7] Add Authorization and messages

--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,12 @@ axios.interceptors.response.use(
     if (error?.response?.status === 400) {
       alert(error.response.data?.data);
     }
+    if (error.response.status === 401) {
+      alert('Unauthorized. You should authorize to perform this action.');
+    }
+    if (error.response.status === 403) {
+      alert('Forbidden. You should have access to perform this action.');
+    }
 
     return Promise.reject(error?.response ?? error);
   }
