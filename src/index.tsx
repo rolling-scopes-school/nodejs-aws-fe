2dc17276097706c664b9733a9698bee10eb9e53c
Dmitrii Esin <dmitrii_esin@epam.com>
2021-06-05 13:05:58 +0300

    authorization added

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'index.css';
-import App from 'components/App/App';
-import {store} from 'store/store';
-import {Provider} from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.css";
+import App from "components/App/App";
+import { store } from "store/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import axios from 'axios';
+import axios from "axios";
 
 axios.interceptors.response.use(
-  response => {
+  (response) => {
     return response;
   },
-  function(error) {
+  function (error) {
     if (error?.response?.status === 400) {
       alert(error.response.data?.data);
+    }
+
+    if (error?.response?.status === 401 || error?.response?.status === 403) {
+      alert(error.response.data?.message);
     }
 
     return Promise.reject(error?.response ?? error);
@@ -24,11 +28,11 @@ axios.interceptors.response.use(
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline/>
-      <App/>
+      <CssBaseline />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
