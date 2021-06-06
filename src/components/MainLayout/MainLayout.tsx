c5250f6d3abe37d510ecb82bc644ed568ee8fb86
Javokhir Achilov <javokhir_achilov@epam.com>
2021-06-07 01:24:57 +0500

    feature: update default store name

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        Achiloves store
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
