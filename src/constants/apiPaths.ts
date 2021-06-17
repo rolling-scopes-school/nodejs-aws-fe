d90eefd9829e18b3107bc5fe212f20744e5e2424
Ivan Piankov <Ivan_Piankov@epam.com>
2021-06-17 23:44:03 +0300

    task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://qst3gt0e20.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://vmle7gk5m6.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://6ux44zy8w5.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
