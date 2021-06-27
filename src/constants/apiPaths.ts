58746d488df6f450a595babaec6f0f1d4cb4ac9d
Yahor Sheinikau <sheynikovea@gmail.com>
2021-06-27 18:50:09 +0300

    feat: Add cart-api url to the apiPaths.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -7,7 +7,7 @@ const API_PATHS = {
   order: `https://${null}.execute-api.eu-west-1.amazonaws.com/dev`,
   import: `https://${importServiceHash}.execute-api.eu-west-1.amazonaws.com/dev`,
   bff: `https://${null}.execute-api.eu-west-1.amazonaws.com/dev`,
-  cart: `https://${null}.execute-api.eu-west-1.amazonaws.com/dev`,
+  cart: `http://naooki-cart-api-dev.eu-west-1.elasticbeanstalk.com/api`,
 };
 
 export default API_PATHS;
