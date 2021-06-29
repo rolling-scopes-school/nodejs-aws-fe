bbf6f956fd35fb8c0acffed8b21fb37551f48537
Aliaksandr Antropau <aliaksandr_antropau@epam.com>
2021-06-29 15:46:52 +0300

    add cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://h9dbdy66g3.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://antropovalexandr-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
   courses: 'https://92ah7k8kbj.execute-api.eu-west-1.amazonaws.com/dev/products',
 };
 
