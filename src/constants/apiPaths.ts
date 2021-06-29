d6c138ca2fafdafac158394e2460416e04d5e41b
Ivan Piankov <Ivan_Piankov@epam.com>
2021-06-29 14:09:17 +0300

    task-9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://vmle7gk5m6.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://deyaerotyi.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://qst3gt0e20.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://vmle7gk5m6.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://deyaerotyi.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://6ux44zy8w5.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
