1814c8b1786e6c69c1a67d906f8e8f20c4dc06b0
Yan Andryshkin <yan_Andryshkin@epam.com>
2021-06-30 21:36:02 +0300

    change apipath for cart to use aws eb

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://m8n7i18cvl.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://1l6v4rauxa.execute-api.eu-west-1.amazonaws.com/dev/products',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  cart: 'http://yan-black-cart-api-devtest.eu-west-1.elasticbeanstalk.com/'
 };
 
 export default API_PATHS;
