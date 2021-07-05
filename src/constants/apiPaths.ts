d831ed87922affadb4052bfe67b8bebcaa0a89bb
Vitalii Puzakov <vitalii_puzakov@epam.com>
2021-07-05 16:47:40 +0300

    Task 8: Docker. AWS Elastic Beanstalk

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://467t7quwt4.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://my-awesome-store-cart-api.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
