df88b5ddd172a6fba13b5c5eb7eaaf33d9e2e573
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-07-06 19:05:10 +0300

    elastic beanstalk url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://oy9v3fpbug.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'aws-rs-cart-api-dev.eba-dcyi7tah.eu-west-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
