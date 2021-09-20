eb7478c1d2044619292cb3638c6edc7e443f2cd4
Raman Mukhin <Kextcf2018@tut.by>
2021-09-20 23:45:04 +0300

    feat: integreated with importService

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://t4mnkz8j1b.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://t4mnkz8j1b.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://t4mnkz8j1b.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://aivwtufhpg.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://t4mnkz8j1b.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://t4mnkz8j1b.execute-api.eu-west-1.amazonaws.com/dev',
 };
