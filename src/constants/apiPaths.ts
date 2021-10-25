67d8b55a39d4d8fe3c77fd73d2db3cd1eeb3208a
Raman Mukhin <Kextcf2018@tut.by>
2021-10-25 20:07:56 +0300

    feat: added cartservice

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://t4mnkz8j1b.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://xdjuo8bbd6.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://t4mnkz8j1b.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://t4mnkz8j1b.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://ramanmukhin-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
