1efd05f0844e13dcf873fca53085e331255a8251
vladimirk <specialkv@gmail.com>
2021-10-25 22:24:35 +0200

    cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,13 +1,14 @@
 const api = process.env.REACT_APP_API;
 const importApi = process.env.REACT_APP_API_IMPORT;
+const cartApi = process.env.REACT_APP_CART_API;
 
 const API_PATHS = {
   products: `${api}/products`,
   product: `${api}/product/{id}`,
   import: `${importApi}/import`,
+  cart: `${cartApi}`,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
