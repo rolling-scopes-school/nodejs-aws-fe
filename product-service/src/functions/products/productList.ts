f55c4017bf12d8abb13515b8242eeda1d568109e
Ajay Nallanagula <ajay.nallanagulla@ukg.com>
2022-07-18 10:23:23 +0530

    Changed the names for product to test

--- product-service/src/functions/products/productList.ts
+++ product-service/src/functions/products/productList.ts
@@ -1,74 +1,74 @@
 const getProducts = async () => [
   {
-    "count": 18,
-    "description": "AjTwo Added another Product ",
-    "id": "82ca93e2-03f7-4152-bc11-0e74c464a60d",
-    "price": 9.99,
-    "title": "AjTwo"
+    count: 18,
+    description: "AjTwo Added another Product ",
+    id: "82ca93e2-03f7-4152-bc11-0e74c464a60d",
+    price: 9.99,
+    title: "XXXXXXXXXXAjTwo",
   },
   {
-    "count": 4,
-    "description": "Short Product Description1",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-    "price": 2.4,
-    "title": "ProductOne"
+    count: 4,
+    description: "Short Product Description1",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+    price: 2.4,
+    title: "ProductOne_YYYY_ZZZZ",
   },
   {
-    "count": 6,
-    "description": "Short Product Description3",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
-    "price": 10,
-    "title": "ProductNew"
+    count: 6,
+    description: "Short Product Description3",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
+    price: 10,
+    title: "ProductNew",
   },
   {
-    "count": 7,
-    "description": "Short Product Description2",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
-    "price": 23,
-    "title": "ProductTop"
+    count: 7,
+    description: "Short Product Description2",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
+    price: 23,
+    title: "ProductTop",
   },
   {
-    "count": 12,
-    "description": "Short Product Description7",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
-    "price": 15,
-    "title": "ProductTitle"
+    count: 12,
+    description: "Short Product Description7",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+    price: 15,
+    title: "ProductTitle",
   },
   {
-    "count": 7,
-    "description": "Short Product Description2",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
-    "price": 23,
-    "title": "Product"
+    count: 7,
+    description: "Short Product Description2",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+    price: 23,
+    title: "Product",
   },
   {
-    "count": 8,
-    "description": "Short Product Description4",
-    "id": "7567ec4b-b10c-48c5-9345-fc73348a80a1",
-    "price": 15,
-    "title": "ProductTest"
+    count: 8,
+    description: "Short Product Description4",
+    id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+    price: 15,
+    title: "ProductTest",
   },
   {
-    "count": 2,
-    "description": "Short Product Descriptio1",
-    "id": "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
-    "price": 23,
-    "title": "Product2"
+    count: 2,
+    description: "Short Product Descriptio1",
+    id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+    price: 23,
+    title: "Product2",
   },
   {
-    "count": 3,
-    "description": "Short Product Description7",
-    "id": "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-    "price": 15,
-    "title": "ProductName"
+    count: 3,
+    description: "Short Product Description7",
+    id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+    price: 15,
+    title: "ProductName",
   },
   {
-    "count": 15,
-    "description": "added a new Product",
-    "id": "79d595ea-6916-46f6-9989-42fc02d06706",
-    "price": 9.4,
-    "title": "AjOne"
-  }
-]
+    count: 15,
+    description: "added a new Product",
+    id: "79d595ea-6916-46f6-9989-42fc02d06706",
+    price: 9.4,
+    title: "AjOne",
+  },
+];
 
-export default getProducts
+export default getProducts;
