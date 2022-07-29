6306d829a54b3cadbf1c98a7547ecac96bea415d
Ajay Nallanagula <ajay.nallanagulla@ukg.com>
2022-07-29 14:01:20 +0530

    Frontend Integration Task5 Imports

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -39,8 +39,9 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
         }
       })
       console.log('File to upload: ', file.name)
+      //Todo response.data will be a signedUrl
       console.log('Uploading to: ', response.data)
-      const result = await fetch(response.data, {
+      const result = await fetch(response.data.preSignedPutUrl, {
         method: 'PUT',
         body: file
       })
