a569acc7537c93790cd0ac42ca6734985cbfc17e
User <User@DESKTOP-C51DDIB>
2023-05-29 11:01:37 +0500

    some items

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <AppBar position="relative">
-      <Toolbar>
+      <Toolbar sx={{bgcolor: 'green'}}>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
             component={RouterLink}
